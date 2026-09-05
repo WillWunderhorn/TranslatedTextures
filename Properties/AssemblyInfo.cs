76ebb8d68f0623669b007ab4338a6add9cf35358
Wunderhorn <Narushev_pr1.19@st.ithub.ru>
2026-09-05 12:32:25 +0300

    v1.3.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,5 +2,5 @@
 using TranslatedTextures;
 
 [assembly: MelonInfo(typeof(TranslatedTextures.Main), "TranslatedTextures", "1.3.0", "LittleWolfStorm", null)]
-[assembly: MelonGame("Hinterland", "TheLongDark")]
+[assembly: MelonGame("Hinterland", "TheLongDark")] 
 
